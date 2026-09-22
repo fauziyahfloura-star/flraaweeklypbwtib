03b429193635951d79eba12f6f653dc90d7cf401
fauziyahfloura-star <fauziyahfloura@gmail.com>
2026-09-22 15:21:19 +0700

    route, view

---
+++ resources/views/profile.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/styless.css">
+    <title>TEKNOLOGI INFORMASI</title>
+</head>
+<body>
+    <h1>HALAMAN PROFILE - TEKNOLOGI INFORMASI</h1>
+    <P> 
+        Nama : Floura Fauziyah Husen <br>
+        Nim : 13242520045 <br>
+        Prodi : Teknologi Informasi <br>
+    </p>
+
+    <script src="js/alert.js"></script>
+    <img src="images/floura.jpeg"></img>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,17 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view ('home');
+});
+
+Route::get('/Profile', function () {
+    return view ('profile');
+});
+
+Route::get('/berita', function () {
+    return view ('berita');
+});
+
+Route::get('/contact', function () {
+    return view ('contact');
 });
